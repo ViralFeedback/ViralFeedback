5a3d2cae1bdca5de5c9e40a6e7da62714d50a5e5
Jake Hartnell <jake.hartnell@gmail.com>
2020-06-20 19:40:13 -0700

    Add Navbar

--- packages/web/src/components/nav.tsx
+++ packages/web/src/components/nav.tsx
@@ -9,6 +9,71 @@ import { Routes } from '../navigation/routes';
 
 interface INav extends RouteComponentProps {}
 
-const Nav: FunctionComponent<INav> = ({ location }) => {
-    return <div></div>;
+const Nav: FunctionComponent<any> = () => {
+    return (
+        <nav className="navbar is-transparent">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                    <img
+                        src="virus.png"
+                        alt="Viral Feedback"
+                        width="28"
+                        height="28"
+                    />
+                </a>
+                <div className="navbar-burger burger" data-target="navbar">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+
+            <div id="navbar" className="navbar-menu">
+                <div className="navbar-start">
+                    {Routes.map((value, key) => {
+                        return (
+                            <Link
+                                to={value.path}
+                                key={key}
+                                className="navbar-item"
+                            >
+                                {value.iconClassName ? (
+                                    <i
+                                        className={`sidebar-icon ${value.iconClassName}`}
+                                    />
+                                ) : (
+                                    ' '
+                                )}{' '}
+                                {value.name}
+                            </Link>
+                        );
+                    })}
+                </div>
+
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="field is-grouped">
+                            <p className="control">
+                                <a
+                                    className="bd-tw-button button"
+                                    data-social-network="Twitter"
+                                    data-social-action="tweet"
+                                    data-social-target="https://bulma.io"
+                                    target="_blank"
+                                    href="https://twitter.com/intent/tweet?text=Viral Feedback&amp;hashtags=factcheck&amp;url=https://viralfeedback.org"
+                                >
+                                    <span className="icon">
+                                        <i className="fab fa-twitter"></i>
+                                    </span>
+                                    <span>Tweet</span>
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 };
+
+export default Nav;
