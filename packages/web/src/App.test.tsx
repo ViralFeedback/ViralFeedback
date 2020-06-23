0bb31e7ea8d923736cf8b9b324134b0dfb8b2f90
Jake Hartnell <jake.hartnell@gmail.com>
2020-06-22 18:44:45 -0700

    Update test text

--- packages/web/src/App.test.tsx
+++ packages/web/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders app', () => {
     const { getByText } = render(<App />);
     const linkElement = getByText(/Home/i);
     expect(linkElement).toBeInTheDocument();
