7e17e0cbbb8371dee9c71f4bc34af94a44345541
Jake Hartnell <jake.hartnell@gmail.com>
2020-07-25 11:01:37 -0700

    Protection from accidentally running watcher

--- packages/server/src/watcher.ts
+++ packages/server/src/watcher.ts
@@ -1,129 +1,134 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-// Make sure there is a file to read IDs from and save them to
-fs.openSync('./annotation_ids.txt', 'a');
-fs.openSync('./account_api_keys.txt', 'a');
+if (process.env.ENVIRONMENT === 'development') {
+    console.log('Development environment. Not running watcher.');
+} else {
+    // Make sure there is a file to read IDs from and save them to
+    fs.openSync('./annotation_ids.txt', 'a');
+    fs.openSync('./account_api_keys.txt', 'a');
 
-setInterval(async () => {
-    const response = await fetch(
-        `https://hypothes.is/api/search?group=${
-            process.env.HYPOTHESIS_DRAFT_GROUP
-                ? process.env.HYPOTHESIS_DRAFT_GROUP
-                : 'VkvyjiZY'
-        }`,
-        {
-            method: 'get',
-            headers: {
-                Authorization: `Bearer ${process.env.HYPOTHESIS_API_TOKEN}`,
-                'Content-Type': 'application/json'
+    setInterval(async () => {
+        const response = await fetch(
+            `https://hypothes.is/api/search?group=${
+                process.env.HYPOTHESIS_DRAFT_GROUP
+                    ? process.env.HYPOTHESIS_DRAFT_GROUP
+                    : 'VkvyjiZY'
+            }`,
+            {
+                method: 'get',
+                headers: {
+                    Authorization: `Bearer ${process.env.HYPOTHESIS_API_TOKEN}`,
+                    'Content-Type': 'application/json'
+                }
             }
-        }
-    );
+        );
 
-    const annotations = await response.json();
+        const annotations = await response.json();
 
-    const ids = fs
-        .readFileSync('./annotation_ids.txt')
-        .toString('utf-8')
-        .split('\n');
+        const ids = fs
+            .readFileSync('./annotation_ids.txt')
+            .toString('utf-8')
+            .split('\n');
 
-    annotations.rows.map((annotation: any, key: number) => {
-        const tags = annotation.tags;
+        annotations.rows.map((annotation: any, key: number) => {
+            const tags = annotation.tags;
 
-        if (tags.length > 0) {
-            tags.map(async (tag: string, index: number) => {
-                if (tag.toLowerCase() === 'publish') {
-                    if (!ids.includes(annotation.id)) {
-                        console.log('Found an annotation to publish!');
-                        fs.appendFileSync(
-                            './annotation_ids.txt',
-                            annotation.id + '\n'
-                        );
-
-                        // account_api_keys.txt should be formated:
-                        // username, api_key
-                        const accountApiKeys = fs
-                            .readFileSync('./account_api_keys.txt')
-                            .toString('utf-8')
-                            .split('\n');
-
-                        const username:
-                            | undefined
-                            | null
-                            | string = annotation.user
-                            .match(/acct:(.*)@hypothes.is/)[1]
-                            .toLowerCase();
-
-                        // Get user's API key
-                        let apiKey: undefined | null | string;
-                        for (const index in accountApiKeys) {
-                            if (accountApiKeys[index] !== '') {
-                                if (
-                                    accountApiKeys[index]
-                                        .match(/(.*),\s/)![1]
-                                        .toLowerCase() === username
-                                ) {
-                                    apiKey = accountApiKeys[index].match(
-                                        /,\s(.*)/
-                                    )![1];
-                                }
-                            }
-                        }
-
-                        console.log(apiKey);
-
-                        if (apiKey) {
-                            console.log(
-                                `Publishing annotation ${annotation.id} by ${username}`
-                            );
-
-                            // Filter out Publish tag
-                            annotation.tags = annotation.tags.filter(
-                                (tag: string) => tag.toLowerCase() !== 'publish'
-                            );
-
-                            // Add group
-                            annotation.group = process.env
-                                .HYPOTHESIS_PUBLISH_GROUP
-                                ? process.env.HYPOTHESIS_PUBLISH_GROUP
-                                : 'igRizgwB';
-
-                            annotation.permissions.read = process.env
-                                .HYPOTHESIS_PUBLISH_GROUP
-                                ? [
-                                      `group:${process.env.HYPOTHESIS_PUBLISH_GROUP}`
-                                  ]
-                                : ['group:igRizgwB'];
-
-                            const createAnnotationResponse = await fetch(
-                                'https://hypothes.is/api/annotations',
-                                {
-                                    method: 'post',
-                                    body: JSON.stringify(annotation),
-                                    headers: {
-                                        Authorization: `Bearer ${apiKey}`,
-                                        'Content-Type': 'application/json'
-                                    }
-                                }
-                            );
-
-                            const createAnnotationResponseJSON = await createAnnotationResponse.json();
-
-                            console.log(createAnnotationResponseJSON);
-
+            if (tags.length > 0) {
+                tags.map(async (tag: string, index: number) => {
+                    if (tag.toLowerCase() === 'publish') {
+                        if (!ids.includes(annotation.id)) {
+                            console.log('Found an annotation to publish!');
                             fs.appendFileSync(
                                 './annotation_ids.txt',
-                                createAnnotationResponseJSON.id + '\n'
+                                annotation.id + '\n'
                             );
-                        } else {
-                            console.error(
-                                `API key not found for ${username}. Check to make sure it is added to the account_api_keys.txt file in the root of the server folder.`
-                            );
+
+                            // account_api_keys.txt should be formated:
+                            // username, api_key
+                            const accountApiKeys = fs
+                                .readFileSync('./account_api_keys.txt')
+                                .toString('utf-8')
+                                .split('\n');
+
+                            const username:
+                                | undefined
+                                | null
+                                | string = annotation.user
+                                .match(/acct:(.*)@hypothes.is/)[1]
+                                .toLowerCase();
+
+                            // Get user's API key
+                            let apiKey: undefined | null | string;
+                            for (const index in accountApiKeys) {
+                                if (accountApiKeys[index] !== '') {
+                                    if (
+                                        accountApiKeys[index]
+                                            .match(/(.*),\s/)![1]
+                                            .toLowerCase() === username
+                                    ) {
+                                        apiKey = accountApiKeys[index].match(
+                                            /,\s(.*)/
+                                        )![1];
+                                    }
+                                }
+                            }
+
+                            console.log(apiKey);
+
+                            if (apiKey) {
+                                console.log(
+                                    `Publishing annotation ${annotation.id} by ${username}`
+                                );
+
+                                // Filter out Publish tag
+                                annotation.tags = annotation.tags.filter(
+                                    (tag: string) =>
+                                        tag.toLowerCase() !== 'publish'
+                                );
+
+                                // Add group
+                                annotation.group = process.env
+                                    .HYPOTHESIS_PUBLISH_GROUP
+                                    ? process.env.HYPOTHESIS_PUBLISH_GROUP
+                                    : 'igRizgwB';
+
+                                annotation.permissions.read = process.env
+                                    .HYPOTHESIS_PUBLISH_GROUP
+                                    ? [
+                                          `group:${process.env.HYPOTHESIS_PUBLISH_GROUP}`
+                                      ]
+                                    : ['group:igRizgwB'];
+
+                                const createAnnotationResponse = await fetch(
+                                    'https://hypothes.is/api/annotations',
+                                    {
+                                        method: 'post',
+                                        body: JSON.stringify(annotation),
+                                        headers: {
+                                            Authorization: `Bearer ${apiKey}`,
+                                            'Content-Type': 'application/json'
+                                        }
+                                    }
+                                );
+
+                                const createAnnotationResponseJSON = await createAnnotationResponse.json();
+
+                                console.log(createAnnotationResponseJSON);
+
+                                fs.appendFileSync(
+                                    './annotation_ids.txt',
+                                    createAnnotationResponseJSON.id + '\n'
+                                );
+                            } else {
+                                console.error(
+                                    `API key not found for ${username}. Check to make sure it is added to the account_api_keys.txt file in the root of the server folder.`
+                                );
+                            }
                         }
                     }
-                }
-            });
-        }
-    });
-}, 300000);
+                });
+            }
+        });
+    }, 300000);
+}
